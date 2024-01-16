1a5e4e6d5f9ea50b6ead6f5a833a378f4aa35919
Mike Nye <mike.nye@gmail.com>
2024-01-16 10:53:27 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+help:
+	echo ""
+	echo "Valid make targets are:"
+	echo "  - uefi-x86-ovf : Outputs a zip file containing .vmx & .vmdk for uefi-x86 platform"
+	echo ""
+
+uefi-x86-ovf:
+	# Clone armbian/build repo if needed
+	if [ ! -d "./armbian-build" ]; then git clone --depth=1 --branch=v23.11 https://github.com/armbian/build ./armbian-build; fi
+	# Copy armbian customisations
+	cp -Rv ./armbian/* ./armbian-build/
+	# Run armbian build
+	cd ./armbian-build && ./compile.sh build BOARD=uefi-x86 ENABLE_EXTENSIONS=image-output-ovf planewatcher
+
+clean:
+	# Remove armbian/build
+	rm -r ./armbian-build
---
+++ armbian/userpatches/customize-image.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+function deploy_from_git_repo() {(
+    # arguements
+    BRANCH="$1"
+    REPO="$2"
+    CLONEDIR="$3"
+    read -a BUILDDEPS <<< "$4"
+    read -a DEPS <<< "$5"
+    INSTALLFUNC="$6"
+
+    BUILDDEPS_TO_REMOVE=()
+
+    set -e
+
+    # temporarily install build deps
+    for pkg in "${BUILDDEPS[@]}"; do
+        # check if package installed
+        if ! dpkg -l "${PACKAGE}" > /dev/null 2>&1; then
+            # package is not installed, install it and mark for uninstallation after build
+            apt-get install --no-install-recommends -y "${pkg}"
+            BUILDDEPS_TO_REMOVE+=("${pkg}")
+        fi
+    done
+
+    # install deps
+    for pkg in "${DEPS[@]}"; do
+        apt-get install --no-install-recommends -y "${pkg}"
+    done
+
+    # clone repo
+    git clone --depth 1 --branch "${BRANCH}" "${REPO}" "${CLONEDIR}"
+
+    # run install tasks
+    ldconfig
+    $INSTALLFUNC
+
+    # uninstall build deps that were installed for this build
+    for pkg in "${BUILDDEPS_TO_REMOVE[@]}"; do
+        apt-get remove -y "${pkg}"
+    done
+    apt-get autoremove -y
+
+    # TODO: clean up: remove repo dir
+    
+)}
+
+function build_rtlsdr ()
+{(
+    set -e
+    mkdir -p "$CLONEDIR"/build
+    pushd "$CLONEDIR"/build
+    LD_LIBRARY_PATH="/usr/include" cmake \
+        ../ \
+        -DINSTALL_UDEV_RULES=ON \
+        -DDETACH_KERNEL_DRIVER=ON \
+        -DENABLE_ZEROCOPY=ON
+    make
+    make install
+    ldconfig
+    popd
+)}
+
+
+
+
+# BRANCH=v4.0.2
+# REPO=https://github.com/Mictronics/readsb-protobuf.git
+# CLONEDIR=/src/Mictronics/readsb-protobuf
+# BUILDDEPS=('build-essential' 'protobuf-c-compiler' 'libprotobuf-c-dev' 'librrd-dev' 'libncurses-dev')
+# DEPS=('libncurses6' 'libprotobuf-c1')
+
+
+# deploy rtl-sdr
+deploy_from_git_repo \
+    "v2.0.1" \
+    "https://gitea.osmocom.org/sdr/rtl-sdr.git" \
+    "/src/rtl-sdr" \
+    "build-essential cmake git libusb-1.0-0-dev pkgconf" \
+    "" \
+    build_rtlsdr
