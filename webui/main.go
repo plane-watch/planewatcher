981f7f92e6fb62a35ef507865458e108ca5824b2
Mike Nye <mike.nye@gmail.com>
2024-01-17 17:55:30 +0800

    .

--- webui/main.go
+++ webui/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/plane-watch/planewatcher/webui/lib/network"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 )
 
 func main() {
@@ -14,10 +16,15 @@ func main() {
 	}
 
 	for _, l := range ll {
+		ips, err := netlink.AddrList(l, unix.AF_INET)
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println(
 			l.Attrs().Index,
 			l.Attrs().Name,
-			l.Attrs().OperState,
+			l.Attrs().OperState.String(),
+			ips,
 		)
 		// fmt.Println("attrs", *l.Attrs())
 		// fmt.Println(fmt.Sprintf("type %s", l.Type()))
