24ac06d87cdc98f8540ffc303a6eaac6420f261f
Mike Nye <mike.nye@gmail.com>
2024-01-17 17:33:34 +0800

    .

--- webui/main.go
+++ webui/main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/vishvananda/netlink"
 )
 
+// returns a list of network devices (excludes loopback and bridge etc)
+func getNetworkDevices() ([]netlink.Link, error) {
+
+	// get list if network interfaces
+	ll, err := netlink.LinkList()
+	if err != nil {
+		return []netlink.Link{}, err
+	}
+
+	// remove loopback
+	llDevicesOnly := []netlink.Link{}
+	for _, l := range ll {
+		if l.Type() == "device" {
+			if l.Attrs().Flags|net.FlagLoopback != net.FlagLoopback {
+				llDevicesOnly = append(llDevicesOnly, l)
+			}
+		}
+	}
+
+	// return sanitised list
+	return llDevicesOnly, nil
+}
+
 func main() {
 
-	ll, err := netlink.LinkList()
+	ll, err := getNetworkDevices()
 	if err != nil {
 		panic(err)
 	}
